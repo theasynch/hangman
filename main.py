c939d99e262b3ea3ca401948fd441dc0977cba91
theasynch <92038575+theasynchronus@users.noreply.github.com>
2024-01-06 09:52:35 +0530

    Update main.py
    Main initializing code part of the program. It runs the game and its functions.

--- main.py
+++ main.py
@@ -133,3 +133,15 @@ def display_hangman(tries):
                 """
     ]
     return stages[tries]
+
+
+def main():
+    word = get_word()
+    play(word)
+    while input("Play Again? (Y/N) ").upper() == "Y":
+        word = get_word()
+        play(word)
+
+
+if __name__ == "__main__":
+    main()
